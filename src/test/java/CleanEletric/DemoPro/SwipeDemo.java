fe75f94d76d4c1237ef66febd5046910df3833a1
SatishBhone <103448764+SatishBhone@users.noreply.github.com>
2023-12-20 19:37:41 +0530

    SwipeCode is Added

---
+++ src/test/java/CleanEletric/DemoPro/SwipeDemo.java
@@ -0,0 +1,45 @@
+package CleanEletric.DemoPro;
+
+
+import static org.testng.Assert.assertEquals;
+
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.google.common.collect.ImmutableMap;
+
+import io.appium.java_client.AppiumBy;
+
+public class SwipeDemo extends BaseTest{
+	
+	
+	@Test
+	public void SwipeDemoTest() throws MalformedURLException, InterruptedException 
+	{
+		// Scrolling Action
+	    driver.findElement(AppiumBy.accessibilityId("Views")).click();
+	    driver.findElement(AppiumBy.accessibilityId("Gallery")).click();
+	    driver.findElement(AppiumBy.accessibilityId("1. Photos")).click();
+	    WebElement firstImage = driver.findElement(By.xpath("(//android.widget.ImageView)[1]"));
+	    Assert.assertEquals(driver.findElement(By.xpath("(//android.widget.ImageView)[1]")).getAttribute("focusable"),"true");
+	     //Swipe
+	    
+	 // Java
+	    ((JavascriptExecutor) driver).executeScript("mobile: swipeGesture", ImmutableMap.of(
+	      "elementId",((RemoteWebElement)firstImage).getId(),
+	        "direction", "left",
+	        "percent", 0.75
+	    ));
+	    
+	    Assert.assertEquals(driver.findElement(By.xpath("(//android.widget.ImageView)[1]")).getAttribute("focusable"),"false");
+	   Thread.sleep(3000);
+	}
+
+	
+}
