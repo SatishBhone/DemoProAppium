072e4c15861325b801714f6c6c945cc2481a7441
SatishBhone <103448764+SatishBhone@users.noreply.github.com>
2024-02-10 18:53:09 +0530

    add Product to Card code added

--- src/test/java/ECommerce/Ecommerce_TestCases.java
+++ src/test/java/ECommerce/Ecommerce_TestCases.java
@@ -1,6 +1,7 @@
 package ECommerce;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
@@ -11,7 +12,7 @@ public class Ecommerce_TestCases extends BaseTest{
    public void Test1() throws InterruptedException 
 	{
 		
-		driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Satish Bhone");
+		//driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Satish Bhone");
 		driver.hideKeyboard();
 		driver.findElement(By.xpath("//android.widget.RadioButton[@text='Female']")).click();
 		driver.findElement(By.id("android:id/text1")).click();
@@ -19,6 +20,11 @@ public class Ecommerce_TestCases extends BaseTest{
 		driver.findElement(By.xpath("//android.widget.TextView[@text='Argentina']")).click();
 		Thread.sleep(3000);
 		driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+	    String toastMessage = driver.findElement(By.xpath("(//android.widget.Toast)[1]")).getAttribute("name");
+	    Assert.assertEquals(toastMessage, "Please enter your name");
+	    
+	    System.out.println("Test Run Succesuflly");
+		
 		
 	}
 }
